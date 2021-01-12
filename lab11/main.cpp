b7773860b25a831e6bce2c9687a9961cd7fda44e
Darina K <40147658+HollisLynch@users.noreply.github.com>
2021-01-12 15:47:21 +0100

    Update main.cpp

--- lab11/main.cpp
+++ lab11/main.cpp
@@ -8,16 +8,23 @@ std::mt19937 rg(r());
 
 using namespace std;
 
+class Activation {
+public:
+    static double f_tanh(double x) {
+        return tanh(x);
+    };
+};
+
 class Neuron {
 public:
     vector<double> weights;
     vector<double> inputs;
 
     Neuron();
-    double f(double x);
     double fprim(double x);
     vector<double> set_weights(int n);
     void train(int epochs);
+    void test(vector<double> weights);
 };
 
 Neuron::Neuron() {
@@ -26,7 +33,6 @@ Neuron::Neuron() {
 vector<double> Neuron::set_weights(int n) {
 
     uniform_real_distribution<double> distribution(0.0,1.0);
-
     for (int i = 0; i < n; i++)
     {
         weights.push_back(distribution(rg));
@@ -34,17 +40,16 @@ vector<double> Neuron::set_weights(int n) {
     return weights;
 }
 
-double Neuron::f(double x) {
-    return tanh(x);
-}
-
 double Neuron::fprim(double x) {
-    return pow(1-f(x), 2);
+    return pow(1-Activation::f_tanh(x), 2);
 }
 
 void Neuron::train(int epochs) {
     for (int i = 0; i<epochs; i++) {
     }
+}
+
+void Neuron::test(vector<double> weights) {
 }
 
 int main() {
